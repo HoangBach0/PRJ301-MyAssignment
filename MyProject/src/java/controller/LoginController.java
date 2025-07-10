b8aa5789503042b3e0961b216a3bec8e38874678
Bach <hoangbach1806cp@gmail.com>
2025-07-10 15:58:29 +0700

    login and web.xml
    handle login request with servlet
    and xml configuration

---
+++ MyProject/src/java/controller/LoginController.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import dal.AccountDBContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import model.Account;
+
+/**
+ *
+ * @author hoang
+ */
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        AccountDBContext dbContext = new AccountDBContext();
+        Account account = dbContext.get(username, password);
+
+        if (account != null) {
+            HttpSession session = req.getSession();
+            session.setAttribute("account", account);
+            resp.getWriter().println("Login successfully! "+ account.getUsername());
+        } else {
+            req.getRequestDispatcher("notification.html").forward(req, resp);
+        }
+    }
+    
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/login.jsp").forward(req, resp);
+    }
+    
+}
